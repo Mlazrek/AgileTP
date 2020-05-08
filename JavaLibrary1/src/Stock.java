7547a22c2590571181baa6457f04629ea3ac7270
Mlazrek <mariam.lazrek@dauphine.eu>
2020-05-08 18:37:43 +0200

    Create Stock.java

---
+++ JavaLibrary1/src/Stock.java
@@ -0,0 +1,51 @@
+/**
+ * Décrivez votre classe Stock ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+    public class Stock{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    public boolean disponible;
+    public boolean Substituable;
+    public int quantite;
+    public Product product1;
+    public Product product2;
+
+    /**
+     * Constructeur d'objets de classe Stock
+     */
+    public Stock()
+    {
+        // initialisation des variables d'instance
+    }
+
+    public int getQuantite() {
+        return quantite ;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+    
+         
+    /**
+     * un exemple methode alimentation de Stock
+     *
+     * @param  quantite dup Stock   le paramètre de la méthode
+     * @return     quantite disponible dans le Stock
+     */
+    
+      
+       
+    public int alimenterStock(int quantite)
+    {
+        // Insérez votre code ici
+        this.quantite += quantite;
+        if (this.quantite>0){ 
+            this.disponible = true;
+        }
+        
+        return quantite;
+    }}
+
