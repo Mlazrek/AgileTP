88e818084632a0d6dc033bc08a7fda2905e6f50b
Mlazrek <mariam.lazrek@dauphine.eu>
2020-05-08 18:37:37 +0200

    Create StockTestTest.java

---
+++ JavaLibrary1/src/StockTestTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hp
+ */
+public class StockTestTest {
+    
+    public StockTestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of setUp method, of class StockTest.
+     */
+    @Test
+    public void testSetUp() {
+        System.out.println("setUp");
+        StockTest instance = new StockTest();
+        instance.setUp();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of tearDown method, of class StockTest.
+     */
+    @Test
+    public void testTearDown() {
+        System.out.println("tearDown");
+        StockTest instance = new StockTest();
+        instance.tearDown();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testisSubstituable method, of class StockTest.
+     */
+    @Test
+    public void testTestisSubstituable() {
+        System.out.println("testisSubstituable");
+        StockTest instance = new StockTest();
+        instance.testisSubstituable();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        
+    }
+    
+}
